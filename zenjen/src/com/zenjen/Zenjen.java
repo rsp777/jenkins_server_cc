d1fae316507937c6254b70f70c575b1bdcf5bfca
RP57547 <RP57547@TRNW51NLPC0136.ind.zensar.com>
2019-09-09 12:13:58 +0530

    zenjen

---
+++ zenjen/src/com/zenjen/Zenjen.java
@@ -0,0 +1,12 @@
+package com.zenjen;
+
+public class Zenjen {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("additon of two numbers : "+a+b);
+
+	}
+
+}
